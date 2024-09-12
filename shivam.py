063ac2235862351449651da584dfda1eec2f2d87
Shivam raj <rajshivam05012003@gmail.com>
2024-09-12 23:13:03 +0530

    2nd commit

--- shivam.py
+++ shivam.py
@@ -1,1 +1,71 @@
-print("Hello Shivam")
+"""
+name='shivam'
+age=21
+print(name+'is a boy')
+# you can't concatnate string and integer in python
+print(name+'is',age)
+
+print('Hi\n How are you')
+print(name[0])
+#string..
+name='shivam'
+
+print(name.lower().islower())
+print(len(name))
+print(name.index('i'))
+print(name.replace('s','m'))
+
+
+#number
+number=78
+print(78+22.934)
+number1=str(number)
+print('number is'+number1)
+print(min(1,2,4,6))
+print(max(5,8,9,16))
+
+from math import *
+print('The square root of number is',sqrt(196))
+
+
+name=78
+print(name)
+number=input('Input Your Number: ')
+print(number)
+
+sentence=input('Enter your sentence: ')
+print('your sentence is: '+sentence)
+countries=['united kingdom','ghana','nigeria']
+print(countries[2])
+print(countries[2][0])
+print(countries[0:2])
+countries[0]='India'
+print(countries)
+"""
+countries=list(('nigeria',34,False))
+countries2=['nigeria',34,False]
+print(type(countries))
+print(type(countries2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
