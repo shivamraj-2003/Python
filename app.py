19698bb2891badf4aec0ae396a6106a7dffc0904
Shivam raj <rajshivam05012003@gmail.com>
2024-09-13 14:26:27 +0530

    Created login and signup page using python

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 #Building a simple login and signup pageusing Python
+print('Create account now')
+username=input('Enter username: ')
+password=input('Enter password: ')
+print('Your account has been created successfully')
+print("Login now!")
+
+username2=input('Enter username: ')
+password2=input('Enter password: ')
+
+if username==username2 and password==password2:
+    print('Logged in successsfully')
+else:
+    print('Inavalid credentials')
